da33d5b9c94f5385a4e60dc1886ff5cf1d505397
Jess <jnoriegaless@gmail.com>
2019-09-01 23:35:48 +0000

    refactor

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,236 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import Axios from 'axios';
-import FilterBar from './components/Filters/FilterBar';
-import Article from './components/Articles/Article';
-const uuidv4 = require('uuid/v4');
-// import filterSelector from './helpers/filter_selector';
-
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Login from './components/Steps/Login'
 import Registration from './components/Steps/Registration'
+import Homepage from './Homepage'
 
-const db = {
-const dummyAcc = {
-  community: {
-    id: 1,
-    name: "coolest beehive",
-    location: "h3h"
-  },
-  household: [{
-    id: 1,
-    community_id: 1, 
-    address: "1489 Norton crt",
-    city: "Vancouver",
-    province: "BC",
-    postal_code: "h3h 1p2"
-  }],
-  user: [
-  {
-    id: 2,
-    household_id: 1,
-    first_name: "Nelly",
-    last_name: "Main",
-    password: "Password",
-    password_confirmation: "Password",
-    profile_pic: "url to a pic",
-    phone_number: "1234567890",
-    bio: "short description of who I am",
-    private: true
-  },
-  {
-    id: 3,
-    household_id: 1,
-    first_name: "Jess",
-    last_name: "N-L",
-    password: "Password",
-    password_confirmation: "Password",
-    profile_pic: "url to a pic",
-    phone_number: "1234567890",
-    bio: "short description of who I am",
-    private: true
-  },
-]
-}
-
-const dummyArticles = [{
-  events:[{
-    id: 1,
-    owner:{
-      id: 3,
-      first_name:"jess"
-    },
-    description: "this is probably a birthday party or somehting",
-    location: "here's where we are",
-    title: "my 35th",
-    cancelled: false,
-    archive: true,
-    comments:[{
-    id: 1,
-    owner:{
-      id:1,
-      first_name: "duncan"
-    },
-    text: "I would love to go to your birthday",
-    notice_id: null,
-    offer_request_id: null, 
-    event_id: 1
-    }]
-  },
-  {
-    id: 2,
-    owner:{
-      id: 1,
-      first_name:"duncan"
-    },
-    description: "I'm hosting game night",
-    location: "here's where we are",
-    title: "games and beer",
-    cancelled: false,
-    archive: true,
-    comments:[{
-    id: 2,
-    owner:{
-      id: 2,
-      first_name: "nelly"
-    },
-    text: "can I bring wine instead?",
-    notice_id: null,
-    offer_request_id: null, 
-    event_id: 2
-    }, 
-    {
-    id:3, 
-    owner: {
-      id: 3,
-      first_name: "jess"
-    },
-    text: "don't think I can make it :(",
-    notice_id: null,
-    offer_request_id: null, 
-    event_id: 2
-    }]
-  }]
-},
-{
-    // ////
-  notices:[{
-    id: 1,
-    owner:{
-      id: 3,
-      first_name:"jess"
-    },
-    description: "This is the body of the notice",
-    title: "I lost my dog",
-    cancelled: false,
-    archive: true,
-    comments:[{
-    id: 1,
-    owner:{
-      id:1,
-      first_name: "duncan"
-    },
-    text: "I saw him on 5th!",
-    notice_id: null,
-    offer_request_id: null, 
-    event_id: 1
-    }]
-  },
-  {
-    id: 2,
-    owner:{
-      id: 1,
-      first_name:"duncan"
-    },
-    description: "I'm hosting game night",
-    location: "here's where we are",
-    title: "games and beer",
-    cancelled: false,
-    archive: true,
-    comments:[{
-    id: 2,
-    owner:{
-      id: 2,
-      first_name: "nelly"
-    },
-    text: "can I bring wine instead?",
-    notice_id: null,
-    offer_request_id: null, 
-    event_id: 2
-    }, 
-    {
-    id:3, 
-    owner: {
-      id: 3,
-      first_name: "jess"
-    },
-    text: "don't think I can make it :(",
-    notice_id: null,
-    offer_request_id: null, 
-    event_id: 2
-    }]
-  }]
-}
-]
-
-function App() {
-  const [articles, setArticles] = useState(dummyArticles)
-  const [filter, setFilter] = useState()
-  const [account, setAccount] = useState(dummyAcc)
-
-  // useEffect(() => {
-  //   axios.get("/test")
-  //     .then(testData => {
-  //       setArticles(testData.rows)
-  //     })
-  //     .catch(err => {
-  //       console.log(err)
-  //     })
-  // })
-
-
+export default function App() {
   return (
     <div className="App">
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/registration">Registration</Link>
-            </li>
-          </ul>
+      <BrowserRouter>
+        <Switch>
+          <div>
+            <ul>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/registration">Registration</Link>
+              </li>
+            </ul>
 
-          <hr />
+            <hr />
 
-          <Route path="/login" component={Login} />
-          <Route path="/registration" component={Registration} />
-        </div>
-      </Router>
+            <Route path="/login" component={Login} />
+            <Route path="/registration" component={Registration} />
+          </div>
+        </Switch>
+      </BrowserRouter>
+      <Homepage></Homepage>
 
-      <header className="App-header">
-        <div>Hypothetical navbar for: {account.community.name}, found at: {account.community.location} </div>
-        <button onClick={event => console.log(filter)}>Current Filter</button>
-        <div>Hello {account.user[0].first_name} </div>
-        {/* pass down the onSelect(setFilter) function which is handed to filters then button.js, and the current filter so FilterBar knows which filter to highlight */}
-        <div>
-          <FilterBar onSelect={setFilter} filter={filter}/>
-        </div>
-        {/* map must be handed an array from articles hook, once recieved in Article it will be identified and the apropriate article component will be rendered */}
-        <div className="container--articles">{articles.map(article => {
-          console.log(article)
-          return (
-            <Article
-              key={uuidv4(6)}
-              article={article}
-            />
-          );
-        })}
-        </div>
-      </header>
     </div>
   );
 }
-
-export default App;
