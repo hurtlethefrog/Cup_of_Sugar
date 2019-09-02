f8a3833980d574d0ae78ee784af399556d0e2a2a
Jess <jnoriegaless@gmail.com>
2019-09-02 03:17:46 +0000

    needs revision

--- frontend/client/src/components/Steps/Registration.js
+++ frontend/client/src/components/Steps/Registration.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+require('dotenv').config();
+
+let G_API_KEY = process.env.G_API_KEY;
 
 let currentLocation = selected => {
   return new Promise((resolve, reject) => {
@@ -9,17 +12,14 @@ let currentLocation = selected => {
 
 currentLocation()
   .then(position => {
-    console.log(position);
-    console.log("position.coords:", position.coords);
     console.log("position.coords.latitude:", position.coords.latitude);
     console.log("position.coords.longitude:", position.coords.longitude);
     axios
       .get(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=YOUR_API_KEY`
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=${G_API_KEY}`
       )
       .then(res => {
-        console.log(res);
-        console.log("res#################:", res);
+        console.log("res", res);
       });
   })
   .catch(err => {
@@ -73,7 +73,7 @@ export default function Registration(props) {
           <input placeholder="Province" {...province} required />
           <input type="submit" value="Submit" />
         </form>
-        <input value="" onChange="" />
+        {/* <input value="" onChange="" /> */}
       </section>
     </main>
   );
