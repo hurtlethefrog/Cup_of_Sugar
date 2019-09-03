90c894f0bb268d069279df17998d0d6df98f5ec7
Nelly Main <nellymain@Nellys-MacBook-Air.local>
2019-09-03 11:42:46 -0400

    format date/time on notice

--- frontend/client/src/components/Articles/Notice.js
+++ frontend/client/src/components/Articles/Notice.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
-
+import { dateFormatter, timeAgo } from "../../helper"
 import "./styles.css";
+const moment = require('moment')
 
 export default function Notice(props) {
   const [state, setState] = useState(false)
@@ -12,7 +13,7 @@ export default function Notice(props) {
         <img src={comment.profile_pic}/>
     <div className="article--userinfo">{comment.first_name} {comment.last_name[0].toUpperCase()+ "." }</div>
     <div className="comment--content">{comment.comment}</div>
-    <div className="hideable--date">{comment.created_at}</div>
+    <div className="hideable--date">{timeAgo(comment.created_at)}</div>
     </div>
     )
   })
@@ -22,6 +23,7 @@ export default function Notice(props) {
       <div className="article-icon">N</div>
       <div className="article--title">{props.article.title}</div>
       <div className="article--description">{props.article.description}</div>
+      <div className="article--date">{dateFormatter(props.article.created_at)}</div>
       {state && parsedComments}
     </article>
   );
---
+++ frontend/client/src/helper.js
@@ -0,0 +1,12 @@
+const moment = require('moment')
+
+export function dateFormatter(datetime) {
+  let date = datetime.slice(0,10)
+
+  return moment(date).format("MMM Do") 
+}
+
+export function timeAgo(datetime) {
+
+  return moment(datetime).fromNow()
+}
