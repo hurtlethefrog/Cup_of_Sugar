72c95fd68bd084debe76bc24d35b1a2060754316
Nelly31 <nelly_main@hotmail.com>
2019-09-03 23:50:19 +0000

    working on adding data limit

--- backend/app/controllers/api/events_controller.rb
+++ backend/app/controllers/api/events_controller.rb
@@ -1,17 +1,18 @@
 class Api::EventsController < ApplicationController
 
+include Pagy::Backend
+
   before_action :set_event
 
   def index
 
   @events = Event.order(created_at: :desc)
 
-
   @events = Event.find_by_sql("SELECT events.*, users.id as user_id, first_name, last_name, profile_pic FROM events JOIN users ON events.owner_id = users.id ORDER BY events.created_at DESC")
 
-  @eventComments = Comment.find_by_sql("SELECT comments.*, users.id as user_id, first_name, last_name, profile_pic FROM comments JOIN users on comments.users_id = users.id WHERE events_id IS NOT NULL ORDER BY comments.created_at DESC")
+  # @eventComments = Comment.find_by_sql("SELECT comments.*, users.id as user_id, first_name, last_name, profile_pic FROM comments JOIN users on comments.users_id = users.id WHERE events_id IS NOT NULL ORDER BY comments.created_at DESC")
 
-  @eventAttendees = EventUser.find_by_sql("SELECT users.id as user_id, first_name, last_name, profile_pic, events.* FROM event_users JOIN users ON event_users.users_id = users.id JOIN events ON event_users.events_id = events.id")
+  # @eventAttendees = EventUser.find_by_sql("SELECT users.id as user_id, first_name, last_name, profile_pic, events.* FROM event_users JOIN users ON event_users.users_id = users.id JOIN events ON event_users.events_id = events.id")
 
   @eventsWithCommentsAndAttendees = @events.map {|event|
     modified_event = generate_hash_with_type(event, "event")
@@ -23,7 +24,10 @@ class Api::EventsController < ApplicationController
     event_output
   } 
 
-  render json: @eventsWithCommentsAndAttendees
+  @pagy_a, @items = pagy_array(@eventsWithCommentsAndAttendees)
+
+
+  # render json: @eventsWithCommentsAndAttendees
 
   end
 
