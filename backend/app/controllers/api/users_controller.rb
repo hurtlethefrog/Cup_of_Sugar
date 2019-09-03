2d402d55305071efa6d27802920a778002e149fe
Nelly31 <nelly_main@hotmail.com>
2019-09-03 21:26:11 +0000

    user controller updated to show all user articles

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::UsersController < ApplicationController
+  before_action :set_user
+
   def index
 
     @users = User.all 
@@ -6,4 +8,69 @@ class Api::UsersController < ApplicationController
     render json: @users
 
   end
+
+  # get user/id
+  def show
+    @events = Event.find_by_sql("SELECT events.*, users.id as user_id, first_name, last_name, profile_pic FROM events JOIN users ON events.owner_id = users.id WHERE owner_id = #{@user.id} ORDER BY events.created_at DESC")
+  
+    @eventsWithCommentsAndAttendees = @events.map {|event|
+      modified_event = generate_hash_with_type(event, "event")
+      comments_hash = {:comments => Comment.find_by_sql("SELECT comments.*, users.id as user_id, first_name, last_name, profile_pic FROM comments JOIN users on comments.users_id = users.id WHERE events_id = #{event.id}")}
+      attendees_hash = {:attendees => EventUser.find_by_sql("SELECT users.id as user_id, first_name, last_name, profile_pic FROM event_users JOIN users ON event_users.users_id = users.id JOIN events ON event_users.events_id = events.id WHERE events_id = #{event.id}")}
+  
+      event_output = modified_event.merge(comments_hash).merge(attendees_hash)
+  
+      event_output
+    } 
+
+    @notices = Notice.find_by_sql("SELECT notices.*, users.id as user_id, first_name, last_name, profile_pic FROM notices JOIN users ON notices.user_id = users.id WHERE user_id = #{@user.id} ORDER BY notices.created_at DESC")
+
+    @noticesWithComments = @notices.map {|notice|
+      modified_notice = generate_hash_with_type(notice, "notice")
+      comments_hash = {:comments => Comment.find_by_sql("SELECT comments.*, users.id as user_id, first_name, last_name, profile_pic FROM comments JOIN users on comments.users_id = users.id WHERE notice_id = #{notice.id}")}
+      notice_output = modified_notice.merge(comments_hash)
+
+      notice_output
+   }
+ 
+    @requests = OffersRequest.find_by_sql("SELECT offers_requests.*, users.id as user_id,first_name, last_name,profile_pic FROM offers_requests JOIN users ON offers_requests.owner_id = users.id WHERE offer IS NOT true AND deleted IS NOT true AND owner_id = #{@user.id} ORDER BY offers_requests.created_at DESC")
+    
+    @requestsWithComments = @requests.map {|request|
+      modified_request = generate_hash_with_type(request, "request")
+      comments_hash = {:comments => Comment.find_by_sql("SELECT comments.*, users.id as user_id, first_name, last_name, profile_pic FROM comments JOIN users on comments.users_id = users.id WHERE offers_requests_id = #{request.id} ORDER BY comments.created_at DESC")}
+      request_output = modified_request.merge(comments_hash)
+  
+      request_output
+      }
+
+    @offers = OffersRequest.find_by_sql("SELECT offers_requests.*, users.id as user_id,first_name, last_name, profile_pic FROM offers_requests JOIN users ON offers_requests.owner_id = users.id WHERE offer = true AND deleted IS NOT true AND owner_id = #{@user.id} ORDER BY offers_requests.created_at DESC")
+
+    @offersWithComments = @offers.map {|offer|
+      modified_offer = generate_hash_with_type(offer, "offer")
+      comments_hash = {:comments => Comment.find_by_sql("SELECT comments.*, users.id as user_id, first_name, last_name, profile_pic FROM comments JOIN users on comments.users_id = users.id WHERE offers_requests_id = #{offer.id} ORDER BY comments.created_at DESC")}
+      offer_output = modified_offer.merge(comments_hash)
+
+      offer_output
+    }
+
+    @userArticles = @eventsWithCommentsAndAttendees + @noticesWithComments + @requestsWithComments + @offersWithComments
+    
+    render json: @userArticles
+
+  end
+
+  def generate_hash_with_type(object, type)
+    hash = object.attributes
+    type_property = {:type => type}
+    hash_with_type = hash.merge(type_property)
+
+    return hash_with_type
+  end
+
+  private 
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   
   namespace :api do
     resources :articles, only: [:index]
-    resources :users
     resources :events
     resources :notices, except: [:destroy]
     resources :offers
     resources :requests
     resources :households 
+
+    resources :users do 
+      resources :articles
+    end
+
   end
   
 end
