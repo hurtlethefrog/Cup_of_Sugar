4be08b570863a5b011b2629e1e3eb811a2ce9de2
Jess <jnoriegaless@gmail.com>
2019-09-05 23:36:19 +0000

    minor modification

--- backend/db/migrate/20190905165236_update_users.rb
+++ backend/db/migrate/20190905165236_update_users.rb
@@ -1,6 +1,6 @@
 class UpdateUsers < ActiveRecord::Migration[6.0]
   def change
+    remove_column :users, :password_confirmation
     rename_column :users, :password, :password_digest
-    remove_column :users , :password_confirmation
   end
 end
