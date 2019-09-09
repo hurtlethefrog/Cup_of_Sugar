7fa1e079ed7359c5bc7f5b8152348a335fd08886
Nelly31 <nelly_main@hotmail.com>
2019-09-09 20:40:52 +0000

    add profile pic to rendered return

--- backend/app/controllers/api/comments_controller.rb
+++ backend/app/controllers/api/comments_controller.rb
@@ -1,4 +1,5 @@
 class Api::CommentsController < ApplicationController
+  before_action :set_user, only: [:create]
 
   def index
 
@@ -6,22 +7,24 @@ class Api::CommentsController < ApplicationController
 
   def create
 
-    puts params 
-
-      @comment = Comment.new(comment_params)
-      
-        if @comment.save
-          render json: @comment, status: :created
-        else
-          render json: @ecomment.errors, status: :unprocessable_entity
-        end
+    @comment = Comment.new(comment_params)
+      if @comment.save
+        @comment_to_render = @comment.attributes.merge({"profile_pic" => @user_pic})
+        render json: @comment_to_render, status: :created
+      else
+        render json: @ecomment.errors, status: :unprocessable_entity
+      end
   end
 
-    
   private 
 
   def comment_params
     params.permit(:users_id, :comment, :offers_requests_id, :events_id, :notices_id)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:users_id])
+    @user_pic = User.find_by_sql("SELECT profile_pic FROM users WHERE id = #{@user["id"]}")[0][:profile_pic]
   end
 
 
