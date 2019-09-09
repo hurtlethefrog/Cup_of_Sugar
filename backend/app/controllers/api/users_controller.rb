d85cda53813c5e9e56b4bca81589f628a1d6ea74
Nelly31 <nelly_main@hotmail.com>
2019-09-09 19:54:27 +0000

    add community id to household table

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::UsersController < ApplicationController
   before_action :set_user
-  # before_action :user_params_hash, only: [:create]
-  # before_action :set_community, only: [:create]
+  before_action :postal_code, :set_community, only: [:create]
 
   def index
     @users = User.all 
@@ -9,14 +8,9 @@ class Api::UsersController < ApplicationController
   end
 
   def create
-    puts "**************"
-    puts @user_hash
-    puts "commmmmmmmunity"
-    puts @community
-    puts "**************"
     @user = User.new(user_params)
     @household = Household.create(address_params)
-    @household.update(communities_id: 1)
+    @household.update(communities_id: @community_id)
     @user.update(households_id: @household[:id])
     if @user.save
       render json: @user, status: :created
@@ -35,11 +29,10 @@ class Api::UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
-  # def set_community
-  #   @first_3_postal_code = @user_hash["postal_code"][0,3]
-  #   @community = Community.where("postal_code LIKE (?)", "%#{@first_3_postal_code}%")
-  #   # @community = {:id => Community.find_by_sql("SELECT id FROM communities WHERE postal_code = 'H2T'")}
-  # end
+  def set_community
+    @community = Community.find_by_sql("SELECT id FROM communities WHERE postal_code LIKE '#{@postal_code_3_digits}'")[0]
+    @community_id = @community[:id]
+  end
 
   def user_params
     params.require(:userForm).permit(:first_name, :last_name, :email, :password, :password_confirmation)
@@ -49,8 +42,9 @@ class Api::UsersController < ApplicationController
     params.require(:userForm).permit(:address, :postal_code, :city, :province)
   end
 
-  # def user_params_hash
-  #   @user_hash = params.require(:userForm).permit(:first_name, :last_name, :email, :password, :password_confirmation, :address, :postal_code, :city, :province).to_hash
-  # end
+  def postal_code
+    @postal_code = params.require(:userForm).permit(:postal_code).to_hash
+    @postal_code_3_digits = @postal_code["postal_code"][0,3]
+  end
 
 end
