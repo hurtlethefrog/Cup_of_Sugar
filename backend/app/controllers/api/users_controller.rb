caad92dc1f6aa101a4a0e0c75bcff31c73cba643
Nelly31 <nelly_main@hotmail.com>
2019-09-09 19:06:35 +0000

    creating new household and user with household id

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::UsersController < ApplicationController
   before_action :set_user
-  before_action :user_params_hash, :set_community, only: [:create]
+  # before_action :user_params_hash, only: [:create]
+  # before_action :set_community, only: [:create]
 
   def index
     @users = User.all 
@@ -9,12 +10,13 @@ class Api::UsersController < ApplicationController
 
   def create
     puts "**************"
-    # puts @community
+    puts @user_hash
+    puts "commmmmmmmunity"
+    puts @community
     puts "**************"
     @user = User.new(user_params)
-    @household = Household.create()
-    @household.update(address: @user_hash["first_name"])
-    # @household.update(address: @user_hash["address"], postal_code: @user_hash["postal_code"], city: @user_hash["city"], province: @user_hash["province"], communities_id: @community[:id])
+    @household = Household.create(address_params)
+    @household.update(communities_id: 1)
     @user.update(households_id: @household[:id])
     if @user.save
       render json: @user, status: :created
@@ -33,22 +35,22 @@ class Api::UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
-  def set_community
-    # # @first_3_postal_code = "H2"
-    # # @community = Community.where("postal_code LIKE (?)", "%#{@first_3_postal_code}%")
-    # @community = {:id => Community.find_by_sql("SELECT * FROM communities WHERE postal_code = 'H2T'")}
-  end
-
-  # def user_params
-  #   params.require(:userForm).permit(:first_name, :last_name, :email, :password, :password_confirmation, :address, :postal_code, :city, :province)
+  # def set_community
+  #   @first_3_postal_code = @user_hash["postal_code"][0,3]
+  #   @community = Community.where("postal_code LIKE (?)", "%#{@first_3_postal_code}%")
+  #   # @community = {:id => Community.find_by_sql("SELECT id FROM communities WHERE postal_code = 'H2T'")}
   # end
 
   def user_params
     params.require(:userForm).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
-  def user_params_hash
-    @user_hash = params.require(:userForm).permit(:first_name, :last_name, :email, :password, :password_confirmation, :address, :postal_code, :city, :province).to_hash
+  def address_params
+    params.require(:userForm).permit(:address, :postal_code, :city, :province)
   end
+
+  # def user_params_hash
+  #   @user_hash = params.require(:userForm).permit(:first_name, :last_name, :email, :password, :password_confirmation, :address, :postal_code, :city, :province).to_hash
+  # end
 
 end
