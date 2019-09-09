918ebafb20b5bbec2cc39a255b828c8dbf3929ad
Nelly31 <nelly_main@hotmail.com>
2019-09-09 22:39:41 +0000

    fix comment names

--- backend/app/controllers/api/comments_controller.rb
+++ backend/app/controllers/api/comments_controller.rb
@@ -9,7 +9,9 @@ class Api::CommentsController < ApplicationController
 
     @comment = Comment.new(comment_params)
       if @comment.save
-        @comment_to_render = @comment.attributes.merge({"profile_pic" => @user_pic})
+        @comment_to_render = @comment.attributes.merge({"first_name" => @user["first_name"], "last_name" => @user["last_name"], "profile_pic" => @user["profile_pic"]})
+        puts "*************"
+        p @comment_to_render
         render json: @comment_to_render, status: :created
       else
         render json: @ecomment.errors, status: :unprocessable_entity
@@ -23,8 +25,8 @@ class Api::CommentsController < ApplicationController
   end
 
   def set_user
-    @user = User.find_by(id: params[:users_id])
-    @user_pic = User.find_by_sql("SELECT profile_pic FROM users WHERE id = #{@user["id"]}")[0][:profile_pic]
+    @user = User.select('id AS user_id', :first_name, :last_name, :profile_pic).find_by(id: params[:users_id])
+    # @user_pic = User.find_by_sql("SELECT profile_pic FROM users WHERE id = #{@user["id"]}")[0][:profile_pic]
   end
 
 
