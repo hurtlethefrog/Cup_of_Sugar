708b546d4fceb6a6eca19a23259f23d0fa5e3cfa
Jess <jnoriegaless@gmail.com>
2019-09-10 14:12:07 +0000

    Add current location in process message.

--- frontend/client/src/components/Steps/AutoAddress.js
+++ frontend/client/src/components/Steps/AutoAddress.js
@@ -7,8 +7,6 @@ import axios from "axios";
 require("dotenv").config();
 let REACT_APP_G_API_KEY = process.env.REACT_APP_G_API_KEY;
 
-
-
 export default function AutoAddress(props) {
   const dispatch = useDispatch();
 
@@ -21,12 +19,11 @@ export default function AutoAddress(props) {
 
   const [reduxAutoAddress, updateAutoAddress] = useState(reduxAutoAddressEntry);
 
-
   const [autoAddress, setAutoAddress] = useState("");
 
   // Verifies the geolocation (user's current location)
   let currentLocation = function(selected) {
-    console.log("HEYY")
+    console.log("HEYY");
     // console.log("USEVISUALMODE:", useVisualMode())
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject, selected);
@@ -35,69 +32,62 @@ export default function AutoAddress(props) {
 
   let autoAddressField = null;
 
-// Provide address (reverse geocoding) based on geolocation (user's current coordinates)
+  // Provide address (reverse geocoding) based on geolocation (user's current coordinates)
   currentLocation()
-  .then(position => {
-    console.log("YO")
+    .then(position => {
 
-    console.log("position.coords.latitude:", position.coords.latitude);
-    console.log("position.coords.longitude:", position.coords.longitude);
-    axios
-      .get(
-        // Only for temporary use
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=KEY`
+      console.log("Current latitude:", position.coords.latitude);
+      console.log("Current longitude:", position.coords.longitude);
 
-        // Enable the following and comment out the one above if you want to make request from Google API (results are displayed in the console):
-        // `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=${REACT_APP_G_API_KEY}`
-      )
-      .then(res => {
-        // // This formatted address isn't always ideal data. Perhaps it's better to concatenate pieces of data below (Street Number, Address, etc.)
-        // console.log("Formatted Address:", res.data.results[0].formatted_address);
-        // // Coordinates
-        // console.log("Latitude:", res.data.results[0].geometry.location.lat);
-        // console.log("Longitude:", res.data.results[0].geometry.location.lng);
-        // // Pieces of data for various fields
-        // let address = `${res.data.results[0].address_components[0].long_name} ${res.data.results[0].address_components[1].long_name}`;
-        // let city = res.data.results[0].address_components[2].long_name;
-        // let province = res.data.results[0].address_components[3].long_name;
-        // let country = res.data.results[0].address_components[4].long_name;
-        // let postal_code = res.data.results[0].address_components[5].long_name;
+      axios
+        .get(
+          // Only for temporary use
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=KEY`
 
+          // Enable the following and comment out the one above if you want to make request from Google API (results are displayed in the console):
+          // `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=${REACT_APP_G_API_KEY}`
+        )
+        .then(res => {
+          // let address = `${res.data.results[0].address_components[0].long_name} ${res.data.results[0].address_components[1].long_name}`;
+          // let city = res.data.results[0].address_components[2].long_name;
+          // let province = res.data.results[0].address_components[3].long_name;
+          // let postal_code = res.data.results[0].address_components[5].long_name;
 
-        // autoAddressField = `${address} ${city} ${province} ${country} ${postal_code}`;
-        // setAutoAddress(autoAddressField)
-
-        // reduxAutoAddressEntry = {
-        //   address: address,
-        //   postal_code: postal_code,
-        //   city: city,
-        //   province: province
-        // };
-      
-        // updateAutoAddress(reduxAutoAddressEntry)
-      });
-  })
-  .catch(err => {
-    console.log(err.message);
-  });
+          // autoAddressField = `${address} ${city} ${province} ${postal_code}`;
+          // setAutoAddress(autoAddressField);
 
 
 
+          // updateAutoAddress({
+          //   address: address,
+          //   postal_code: postal_code,
+          //   city: city,
+          //   province: province
+          // });
+          // updateAutoAddress(reduxAutoAddressEntry)
+        });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 
   return (
     <main>
       <section className="">
-        <p>One moment, here you go!</p>
         <ul>
           <li>
-          <input className="disabled-input" placeholder="Address" value={autoAddress} disabled="disabled" />
+            <input
+              className="disabled-input"
+              placeholder="Please wait a moment for autocompletion..."
+              value={autoAddress}
+              disabled="disabled"
+            />
           </li>
-          <li>
-          <button className="btn-edit" onClick={props.onEdit}>Edit</button>
 
-          </li>
+          {/* <li>
+            <button className="btn-edit" onClick={props.onEdit}>Edit</button>
+          </li> */}
         </ul>
-
       </section>
 
       <footer>
