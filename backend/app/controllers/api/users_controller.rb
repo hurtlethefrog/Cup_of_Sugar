80b58cbff7193b330de4540da201850eb480f617
Nelly31 <nelly_main@hotmail.com>
2019-09-10 15:27:09 +0000

    merged in master

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::UsersController < ApplicationController
   before_action :set_user
   # before_action :postal_code, :set_community, only: [:create]
-  before_action :postal_code, only: [:create]
+  # before_action :postal_code, only: [:create]
 
   def index
     @users = User.all 
@@ -11,13 +11,12 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @household = Household.create(address_params)
+    @household.update(communities_id: 1)
     # @household.update(communities_id: @community_id)
     @user.update(households_id: @household[:id])
     if @user.save
       @user_id = @user[:id]
       @user_with_id = @user.attributes.merge({:user_id => @user_id})
-      puts "*******************"
-      puts @user_with_id
       render json: @user_with_id, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -47,9 +46,9 @@ class Api::UsersController < ApplicationController
     params.require(:userForm).permit(:address, :postal_code, :city, :province)
   end
 
-  def postal_code
-    @postal_code = params.require(:userForm).permit(:postal_code).to_hash
-    @postal_code_3_digits = @postal_code["postal_code"][0,3]
-  end
+  # def postal_code
+  #   @postal_code = params.require(:userForm).permit(:postal_code).to_hash
+  #   @postal_code_3_digits = @postal_code["postal_code"][0,3]
+  # end
 
 end
